ef4720472c4773904b9ba102dacd5f2c6970927f
Andrey Bayandin <dontusecascade@gmail.com>
2013-06-25 01:18:29 +0600

    i love bem - 1

---
+++ desktop.bundles/index/index.bemjson.js
@@ -0,0 +1,29 @@
+({
+    block: 'b-page',
+    title: 'Title of the page',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'css', url: '_index.css', ie: false },
+        { elem: 'css', url: '_index', ie: true },
+        { elem: 'meta', attrs: { name: 'description', content: '' }},
+        { elem: 'meta', attrs: { name: 'keywords', content: '' }}
+    ],
+    content:[
+        {
+            block : 'head',
+            content: {
+                block: 'layout',
+                content: [
+                    {
+                        elem: 'left',
+                        content: 'left here'
+                    },
+                    {
+                        elem: 'right',
+                        content: 'right here'
+                    }
+                ]
+            }
+        }
+    ]
+})
